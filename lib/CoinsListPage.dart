0b876bfe57fa16b1fa0f3746eb8fac0afef12912
erfan <erfan01724@gmail.com>
2024-02-01 19:23:36 +0330

    working on CoinListPage

--- lib/CoinsListPage.dart
+++ lib/CoinsListPage.dart
@@ -22,17 +22,38 @@ class _CoinsListPageState extends State<CoinsListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 28, 28, 28),
       body: SafeArea(
-          child: ListView.builder(
-        itemCount: passedCoins!.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              passedCoins![index].name.toString(),
-            ),
-          );
-        },
-      )),
+        child: ListView.builder(
+          itemCount: passedCoins!.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text(
+                passedCoins![index].rank.toString(),
+                style: TextStyle(color: Colors.white),
+              ),
+              trailing: Container(
+                width: 100,
+                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [Text('Cost'), Text('Change')],
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Text('Sec2'),
+                ]),
+              ),
+              title: Text(
+                passedCoins![index].name.toString(),
+                style: TextStyle(color: Color.fromARGB(255, 38, 252, 202)),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
